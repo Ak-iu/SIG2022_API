c152b3276af78b8d47e677a499c66802036c8f72
Evan Mulumba <evans.mllb@gmail.com>
2022-11-25 20:06:40 +0100

    first commit

---
+++ src/main/java/com/m2/sig2022_api/Sig2022ApiApplication.java
@@ -0,0 +1,13 @@
+package com.m2.sig2022_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sig2022ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sig2022ApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/m2/sig2022_api/Sig2022ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m2.sig2022_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sig2022ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
